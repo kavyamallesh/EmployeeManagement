8ed3d16402fdc5a503ca70470fe8f3ebc94fb68b
kavyamallesh <kavyam1502@gmail.com>
2021-03-28 00:57:57 +0800

    fix failing test

--- src/main/java/com/department/hr/employeeManagement/controllers/ExceptionHandlingController.java
+++ src/main/java/com/department/hr/employeeManagement/controllers/ExceptionHandlingController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
 
 import java.io.IOException;
 
@@ -16,7 +17,7 @@ import java.io.IOException;
 @Slf4j
 public class ExceptionHandlingController {
 
-    @ExceptionHandler({BadInputException.class, InvalidFieldException.class, RuntimeException.class, FileFormatException.class, DuplicateDataException.class})
+    @ExceptionHandler({BadInputException.class, MissingServletRequestPartException.class, InvalidFieldException.class, RuntimeException.class, FileFormatException.class, DuplicateDataException.class})
     public ResponseEntity badInputErrorHandler(Exception e) {
         log.error("this is a bad input" + e.getCause().getMessage());
         return ResponseEntity.badRequest().body(e.getCause().getMessage());
